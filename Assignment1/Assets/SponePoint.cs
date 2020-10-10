b91b0abdb9886eaed62850a3282214d37bd1d12c
jack6412 <jack6chang@gmail.com>
2020-10-10 10:35:12 -0400

    testing
    test to sumit the assignment1

---
+++ Assignment1/Assets/SponePoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class SponePoint : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerPrefab;
+
+    static PlayerCharacterController player = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+            player = Instantiate(playerPrefab, transform.position, transform.rotation).GetComponent<PlayerCharacterController>();
+
+
+    }
+}
