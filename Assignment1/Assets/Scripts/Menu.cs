bf8bcf308c75ff8add7cc83f674ddfc351a9ac76
jack6412 <jack6chang@gmail.com>
2020-12-09 11:07:23 -0500

    Main Menu code
    Add Main Menu of exit code

---
+++ Assignment1/Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(tag);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
