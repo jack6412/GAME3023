4454fd5a4907678e91d05d4d66a1dfc23aa7bfd2
jack6412 <jack6chang@gmail.com>
2020-12-10 14:19:04 -0500

    Main menu
    Main Menu able to switch script and able to show student info.

--- Assignment1/Assets/Scripts/Menu.cs
+++ Assignment1/Assets/Scripts/Menu.cs
@@ -5,10 +5,24 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
+
 public class Menu : MonoBehaviour
 {
+    public float WaitTime;
+
+    public GameObject Start_Scream;
+    public GameObject User_Scream;
+
     public void StartGame()
     {
+        User_Scream.gameObject.SetActive(true);
+        Start_Scream.gameObject.SetActive(false);
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(WaitTime);
         SceneManager.LoadScene(tag);
     }
 
